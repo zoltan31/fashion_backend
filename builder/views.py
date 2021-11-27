5528655a8c014dc28ba69063306180743090ff36
Bereczki Norman <bnorman@Bereczki-MacBook-Pro.local>
2021-11-27 14:33:48 +0100

    half implemented func

--- builder/views.py
+++ builder/views.py
@@ -3,7 +3,11 @@
 from builder.models import User, Cloth
 from builder.serializers import UserSerializer, ClothSerializer
 from rest_framework import generics
-
+from django.http import JsonResponse
+from django.utils import timezone
+import datetime as dt
+active_style = 'retro'
+next_style = 'goth'
 #Generic views for User class:
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -24,3 +28,23 @@ class ClothList(generics.ListCreateAPIView):
 class ClothDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cloth.objects.all()
     serializer_class = ClothSerializer
+
+def fv(request):
+    style_list = ['retro', 'hypebeast', 'goth', 'smart-casual',
+    'alt', 'vintage', 'exotic', 'bohemian', 'sporty', 'military']
+    type_list = ['socks', 't-shirt', 'hoodie', 'trousers', 'shirt',
+    'hat', 'pullover', 'footwear', 'accessory']
+    
+    ctr_next = 0
+    expired_type = []
+    clothes = Cloth.objects.all().copy()
+    clothes = filter(lambda i: i.date_of_purchase+dt.timedelta(days=60)<= dt.datetime.now(), clothes)
+    clothes = filter(lambda i: i.style == next_style, clothes)
+    for i in clothes:
+        if i.style == next_style:
+            ctr_next+=1
+            
+    
+
+"clohtes.objecs.all összes ruha, aztán végigiterálok rajta"
+"return: JSON"
